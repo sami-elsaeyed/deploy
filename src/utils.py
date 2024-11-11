97923f5d11e74f13781c7cca2ef525404ec07ca4
sami-elsaeyed <samiabdu.11377@gmail.com>
2024-11-11 14:31:08 -0500

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ from sklearn.metrics import r2_score
 
 
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
+from src.logger import logging
 
 def save_object(file_path, obj):
     try:
@@ -20,11 +22,21 @@ def save_object(file_path, obj):
     
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         for i in models:
             model=models[i]
+            param=params[i]
+
+
+
+            model_gs=GridSearchCV(model, param)
+            model_gs.fit(X_train,y_train)
+
+            logging.info(f"Completed Grid Search for {model}")
+
+            model.set_params(**model_gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred=model.predict(X_train)
